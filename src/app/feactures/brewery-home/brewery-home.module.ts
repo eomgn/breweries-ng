c383e8767e2e6491b72d59b23b3638a07faa42a4
eomgn <matheeusgn21@gmail.com>
2023-09-15 12:56:56 -0300

    style :construction: applying style in card component

--- src/app/feactures/brewery-home/brewery-home.module.ts
+++ src/app/feactures/brewery-home/brewery-home.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// routing
 import { BreweryHomeRoutingModule } from './brewery-home-routing.module';
+
+// modules
+import { SharedModule } from 'src/app/shared/shared.module';
+
+// components
 import { BreweryHomeComponent } from '../brewery-home/brewery-home.component';
 import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [BreweryHomeComponent, CardComponent],
-  imports: [CommonModule, BreweryHomeRoutingModule],
+  imports: [CommonModule, BreweryHomeRoutingModule, SharedModule],
 })
 export class BreweryHomeModule {}
--- src/app/shared/design-system/primeng/primeng.module.ts
+++ src/app/shared/design-system/primeng/primeng.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
-  imports: [CommonModule, ToolbarModule, ButtonModule],
-  exports: [ToolbarModule, ButtonModule],
+  imports: [CommonModule, ToolbarModule, ButtonModule, CardModule],
+  exports: [ToolbarModule, ButtonModule, CardModule],
 })
 export class PrimengModule {}
